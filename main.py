03ac193861148e12f256bfe477234f505d9ddc1f
hajakuja <uqabra@gmail.com>
2019-01-06 14:34:57 +0300

    Downloads everything and saves it as a txt file

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-def main():
-    pass
+def  GetNextLink(content):
+    for tag in content.find_all('a'):
+        if 'Next' in tag.text:
+            return tag.attrs['href']
+    return None
 
-if name == "__main__":
-    main()
+
+def downloadChapters(link):
+    print(link)
+    site = requests.get(link)
+    site.raise_for_status()
+    soup = BeautifulSoup(site.content)
+    content = soup.select('.entry-content')[0]
+    [d.extract() for d in content.findAll('div')]
+    next_link = GetNextLink(content)
+    [d.extract() for d in content.findAll('a')]
+    with open('book.txt', 'ab') as file:
+        file.write("\n\n".encode('utf8'))
+        file.write(soup.select('.entry-title')[0].text.encode('utf8'))
+        file.write("\n\n".encode('utf8'))
+        file.write(content.getText().encode('utf8'))
+    if next_link is not None:
+        downloadChapters(next_link)
+    else:
+        return
+
+
+if __name__ == "__main__":
+    downloadChapters('https://www.parahumans.net/2017/09/11/daybreak-1-1/')
